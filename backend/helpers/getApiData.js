4928a0dbe48a6e95c14138818e5b0b761926b523
Jay Vigilla <JayRVigilla@gmail.com>
2020-11-26 21:58:42 -0800

    WIP routes
    axios call not executing as expected. more attention tomorrow

--- backend/helpers/getApiData.js
+++ backend/helpers/getApiData.js
@@ -7,9 +7,15 @@
 
 const axios = require('axios')
 
+const header = {
+	"x-rapidapi-key": "d0b9f50c04msh96a27d00b76a824p147cb4jsn935f6bf9deda",
+	"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com",
+	"useQueryString": true
+}
+
 async function getApiData(url) {
   try {
-    const response = await axios.get(url)
+    const response = await axios.get(url, header)
     return response.data.posts
   }catch(e){
     console.error(e)
---
+++ backend/routes.js
@@ -0,0 +1,71 @@
+
+const express = require('express');
+const ExpressError = require('./helpers/expressError');
+
+const router = new express.Router();
+
+const API_BASE = 'https://movies-tvshows-data-imdb.p.rapidapi.com/'
+const getApiData = require('./helpers/getApiData');
+
+/**
+ * GET movie titles from query
+ *
+ * () => {
+ *                      title,
+ *    "movie_results": [{
+ *                      year,
+ *                      imdb_id
+ *                      },
+ *                      ...,
+ *                      {}]
+ * }
+ * Response status code: 200
+ * */
+router.get('/search', async function (req, res, next) {
+  try {
+    const q = req.body.q
+    const qString = `title=${q}`
+    const response = await getApiData(`${API_BASE}?${qString}&type=get-movies-by-title`).data
+    return res.json({ response });
+   }catch(err){
+    return next(err);
+  }
+})
+
+
+ /**
+  * GET movie details from IMDB ID
+  *
+  * (success) => {
+  *               title,
+  *               description,
+  *               year,
+  *               release_date,
+  *               imdb_id,
+  *               imdb_rating,
+  *               vote_count,
+  *               popularity,
+  *               youtube_trailer_key,
+  *               rated,
+  *               runtime,
+  *               genres: [],
+  *               stars: [],
+  *               directors:[],
+  *               countries,
+  *               language,
+  *               status,
+  *               status_message
+  *               }
+  */
+router.get('/movie', async function (req, res, next) {
+  try {
+    const q = req.body.q
+    const qString = `imdb=${q}`
+    const response = await getApiData(`${API_BASE}?${qString}&type=get-movie-details`).data
+    return res.json({ response });
+    }catch(err){
+    return next(err);
+  }
+})
+
+module.exports = router;
